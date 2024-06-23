366fb974c6e74bbe75408da70b564e62627bcffd
Ravi Ailani <rvbugs0@gmail.com>
2024-06-22 20:09:47 -0500

    finished with css

--- index.js
+++ index.js
@@ -1,93 +1,95 @@
+var autoscroll = true;
+
 document.addEventListener("DOMContentLoaded", function () {
-
-    getTasks();
-
-    addTaskInputListener();
+  getTasks();
 
 
+
+  addTaskInputListener();
+
+
+  autoscroll = document.getElementById("autoscroll").checked;
+  
 });
 
-function addTaskInputListener(){
-
-    taskInput = document.getElementById("taskInput");
-    taskInput.addEventListener("keyup", function(event) {
-
-            // console.log(event.target.value);
-            if(event.key === "Enter") {
-
-                textContent = event.target.value.trim();
-                if(textContent.length>0){
-                    insertTask(textContent)
-                    event.target.value = "";
-                }
-
-
-            }
-    });
+function autoscrollEvent(){
+  autoscroll = document.getElementById("autoscroll").checked;
+  console.log(autoscroll)
 }
 
-function getTasks(){
-    let taskContainer = document.getElementById("taskContainer");
-    fetch("api.php")
-      .then((response) => {
-        return response.json();
-      })
-      .then((tasks) => {
-        let fullStr = "";
-        tasks.forEach((task) => {
-          let str = "<div class='task'>";
-          str +=
-            "<span>" +
-            task.content +
-            "</span> &nbsp;<span class='deleteBtn' id='" +
-            task.id +
-            "' onclick=deleteTask(" +
-            task.id +
-            ",this) >X</span>";
-          str += "</div>";
-          fullStr += str;
-        });
-        taskContainer.innerHTML = fullStr;
-  
-  
-        
+function addTaskInputListener() {
+  taskInput = document.getElementById("taskInput");
+  taskInput.addEventListener("keyup", function (event) {
+    // console.log(event.target.value);
+    if (event.key === "Enter") {
+      textContent = event.target.value.trim();
+      if (textContent.length > 0) {
+        insertTask(textContent);
+        event.target.value = "";
+      }
+    }
+  });
+}
+
+function getTasks() {
+  let taskContainer = document.getElementById("taskContainer");
+  fetch("data.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((tasks) => {
+      let fullStr = "";
+      tasks.forEach((task) => {
+        let str = "<div class='task'>";
+        str +=
+          "<span>" +
+          task.content +
+          "</span> &nbsp;<span class='deleteBtn' id='" +
+          task.id +
+          "' onclick=deleteTask(" +
+          task.id +
+          ",this) >X</span>";
+        str += "</div>";
+        fullStr += str;
       });
+      taskContainer.innerHTML = fullStr;
+    });
 }
 
 function deleteTask(id, e) {
-  fetch("api.php", { body: JSON.stringify({ id: id }), method: "DELETE" })
-    .then((response) => response.json())
-    .then((response) => {
-      e.parentElement.style.display = "none";
-    });
+  // fetch("api.php", { body: JSON.stringify({ id: id }), method: "DELETE" })
+  //   .then((response) => response.json())
+  //   .then((response) => {
+  //     e.parentElement.style.display = "none";
+  //   });
+
+  e.parentElement.style.display = "none";
 }
 
 function insertTask(content) {
-  fetch("api.php", {
-    body: JSON.stringify({ content: content }),
-    method: "POST",
-  })
-    .then((response) => response.json())
-    .then((task) => {
 
 
+  
+  let taskContainer = document.getElementById("taskContainer");
 
-      let taskContainer = document.getElementById("taskContainer");
+  let children = taskContainer.childNodes;
+  let lastid=taskContainer.childNodes[children.length-1].childNodes[2].id;
+  let task = {content:content,id:lastid+1};
 
-      let str = "<div class='task'>";
-      str +=
-        "<span>" +
-        task.content +
-        "</span> &nbsp;<span class='deleteBtn' id='" +
-        task.id +
-        "' onclick=deleteTask(" +
-        task.id +
-        ",this) >X</span>";
-      str += "</div>";
+  let str = "<div class='task'>";
+  str +=
+    "<span>" +
+    task.content +
+    "</span> &nbsp;<span class='deleteBtn' id='" +
+    task.id +
+    "' onclick=deleteTask(" +
+    task.id +
+    ",this) >X</span>";
+  str += "</div>";
 
-      taskContainer.innerHTML = str+ taskContainer.innerHTML;
+  taskContainer.innerHTML =  taskContainer.innerHTML+str;
 
-
-    });
+  if(autoscroll){
+    taskContainer.scrollTop = taskContainer.scrollHeight;
+  }
 }
-
